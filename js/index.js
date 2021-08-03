8fb6d45feac428ec599963a51828b7b91c491372
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-02 17:07:11 -0700

    finishing up

--- js/index.js
+++ js/index.js
@@ -40,3 +40,52 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+//Nav Data
+const navitem1 = document.querySelector('nav a')
+navitem1.textContent = siteContent["nav"]["nav-item-1"]
+
+const navitem2 = navitem1.nextElementSibling
+navitem2.textContent = siteContent["nav"]["nav-item-2"]
+
+const navitem3 = navitem2.nextElementSibling
+navitem3.textContent = siteContent["nav"]["nav-item-3"]
+
+const navitem4 = navitem3.nextElementSibling
+navitem4.textContent = siteContent["nav"]["nav-item-4"]
+
+const navitem5 = navitem4.nextElementSibling
+navitem5.textContent = siteContent["nav"]["nav-item-5"]
+
+const navitem6 = navitem5.nextElementSibling
+navitem6.textContent = siteContent["nav"]["nav-item-6"]
+
+//Header img
+const headerImg = document.querySelector('.container img')
+headerImg.src = siteContent["nav"]["img-src"]
+
+//Updating H1 including a br and button content 
+
+const ctaTextH1 = document.querySelector(".cta-text h1")
+ctaTextH1.textContent = siteContent["cta"]["h1"]
+
+const ctaTextbutton = document.querySelector(".cta-text button")
+ctaTextbutton.textContent = siteContent["cta"]["button"]
+
+const ctaImg = document.querySelector(".cta img")
+ctaImg.src = siteContent["cta"]["img-src"]
+
+//including the filler text 
+
+const topContentH4 = document.querySelector(".text-content:nth-of-type(1) h4")
+topContentH4.textContent = siteContent["main-content"]["features-h4"]
+
+const topContentP = topContentH4.nextElementSibling
+topContentP.textContent = siteContent["main-content"]["features-content"]
+
+const topContent2H4 = document.querySelector(".text-content:nth-of-type(2) h4")
+topContent2H4.textContent = siteContent["main-content"]["about-h4"]
+
+const topContent2P = topContent2H4.nextElementSibling
+topContent2P.textContent = siteContent["main-content"]["about-content"]
+
+
